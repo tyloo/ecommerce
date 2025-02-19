e2eb622d8bc492c7d75fc016b667782c5cad416e
tyloo <jbonva@gmail.com>
2025-02-19 16:54:10 +0100

    Fixed CartMenu styling
    Signed-off-by: tyloo <jbonva@gmail.com>

--- components/cart-menu.tsx
+++ components/cart-menu.tsx
@@ -11,9 +11,10 @@ import {
   SheetTrigger
 } from '@/components/ui/sheet'
 import { collections } from '@/data/collections'
-import { Minus, Plus, ShoppingBag, X } from 'lucide-react'
+import { ShoppingBag, Trash } from 'lucide-react'
 import Image from 'next/image'
 import { CollectionCard } from './collection-card'
+import { Badge } from './ui/badge'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Progress } from './ui/progress'
@@ -27,12 +28,13 @@ export function CartMenu() {
         </Button>
       </SheetTrigger>
       <SheetContent className='flex h-full w-full flex-col sm:max-w-lg'>
-        <SheetHeader className='space-y-2'>
-          <SheetTitle className='text-xl font-bold'>Mon Panier</SheetTitle>
+        <SheetHeader className='sticky top-0 z-10 space-y-2'>
+          <SheetTitle className='hidden'>Mon Panier</SheetTitle>
           <SheetDescription className='hidden'>Gérez vos articles et passez à la caisse</SheetDescription>
+          <div className='text-xl font-bold'>Mon panier</div>
         </SheetHeader>
-        <div className='flex flex-1 flex-col overflow-hidden'>
-          <div className='bg-background sticky top-0 z-10 pb-4'>
+        <div className='flex flex-1 flex-col overflow-y-auto border-t border-b bg-gray-100'>
+          <div className='p-6'>
             <div className='flex flex-col gap-1 text-sm'>
               <div>Plus que 91,05 € pour bénéficier de la livraison offerte</div>
               <Progress value={58.95} max={150} />
@@ -42,41 +44,55 @@ export function CartMenu() {
               </div>
             </div>
           </div>
-          <div className='flex-1 overflow-y-auto py-4'>
+          <div className='flex-1 p-4'>
             <div className='space-y-4'>
-              {/* Cart Items */}
               <div className='flex items-start justify-between gap-4'>
                 <div className='relative h-24 w-24 flex-shrink-0'>
                   <Image src='/collections/homme-polos.webp' alt='Product' fill className='object-cover' />
                 </div>
                 <div className='flex flex-1 flex-col'>
                   <div className='flex justify-between'>
-                    <div>
-                      <h3 className='font-medium'>Polo Classic Fit</h3>
-                      <p className='text-sm text-gray-500'>Taille: M</p>
+                    <div className='my-3 text-sm'>
+                      <div className='font-medium'>Polo homme</div>
+                      <div className='mb-2 text-gray-500'>5 - L • MANDARINIER 08E • Très bon état</div>
+                      <div className='flex items-center justify-start gap-1'>
+                        <div className='text-muted line-through'>120 €</div>
+
+                        <div className='text-primary font-bold'>54 €</div>
+                        <Badge className='rounded-none px-1'>-55 %</Badge>
+                      </div>
                     </div>
                     <Button variant='ghost' size='icon'>
-                      <X className='h-4 w-4' />
+                      <Trash className='h-4 w-4' />
                     </Button>
                   </div>
-                  <div className='mt-2 flex items-center justify-between'>
-                    <div className='flex items-center gap-2'>
-                      <Button variant='outline' size='icon'>
-                        <Minus className='h-4 w-4' />
-                      </Button>
-                      <span>1</span>
-                      <Button variant='outline' size='icon'>
-                        <Plus className='h-4 w-4' />
-                      </Button>
+                </div>
+              </div>
+              <div className='flex items-start justify-between gap-4'>
+                <div className='relative h-24 w-24 flex-shrink-0'>
+                  <Image src='/collections/homme-polos.webp' alt='Product' fill className='object-cover' />
+                </div>
+                <div className='flex flex-1 flex-col'>
+                  <div className='flex justify-between'>
+                    <div className='my-3 text-sm'>
+                      <div className='font-medium'>Polo homme</div>
+                      <div className='mb-2 text-gray-500'>5 - L • MANDARINIER 08E • Très bon état</div>
+                      <div className='flex items-center justify-start gap-1'>
+                        <div className='text-muted line-through'>120 €</div>
+
+                        <div className='text-primary font-bold'>54 €</div>
+                        <Badge className='rounded-none px-1'>-55 %</Badge>
+                      </div>
                     </div>
-                    <p className='font-medium'>120,00 €</p>
+                    <Button variant='ghost' size='icon'>
+                      <Trash className='h-4 w-4' />
+                    </Button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className='bg-background sticky bottom-0 z-10'>
-            {/* Related Products */}
+          <div className='p-4'>
             <div className='border-t pt-6'>
               <h3 className='mb-4 font-bold uppercase'>Vous aimerez aussi</h3>
               <div className='grid grid-cols-2 gap-4'>
@@ -87,10 +103,10 @@ export function CartMenu() {
             </div>
           </div>
         </div>
-        <SheetFooter>
+        <SheetFooter className='sticky bottom-0 z-10'>
           <div className='grid w-full items-center gap-1.5'>
             <Label htmlFor='picture'>Code promo</Label>
-            <Input id='picture' />
+            <Input id='picture' className='rounded-none' />
           </div>
           <ul className='mb-2 text-sm/10'>
             <li className='flex flex-row items-start justify-between'>
